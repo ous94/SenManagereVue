c40cd1c1b3075522e9f47128f42cb03f7e17c127
Majanny <aisaisa02@gmail.com>
2019-01-15 16:34:06 +0000

    Creation component rechercheEmail

--- src/app/employee/recherche-email/recherche-email.component.ts
+++ src/app/employee/recherche-email/recherche-email.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from 'src/app/Classe/Employee';
+import { EmployeeService } from 'src/app/employee.service';
+import { Router } from '@angular/router';
+import { UploadFileService } from 'src/app/upload-file.service';
 
 @Component({
   selector: 'app-recherche-email',
@@ -7,9 +11,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RechercheEmailComponent implements OnInit {
 
-  constructor() { }
+  rechercherEmail : Employee[];
+  email:String;
+
+//private baseurl: string = 'http://localhost:4201/api/customers';
+constructor(private EmployeeService:EmployeeService,private router:Router,private UploadFileService:UploadFileService) {
+  setTimeout(() => {
+
+    
+  }, 2000
+   );
+ }
 
   ngOnInit() {
+    this.email=null;
   }
 
+   // rechercher par age
+   private searchCustomers() {
+    this.EmployeeService.getEmployerByAdresse(this.email)
+      .subscribe(rechercherEmail => this.rechercherEmail =rechercherEmail);
+  }
+  //button de validation
+  rechercher() {
+    this.searchCustomers();
+  }
+
+  uploadPhotoEmployer(photo:String): String
+  {
+    return this.UploadFileService.getPhoto(photo);
+  }
+
+  // delete employee
+deleteEmploye(employee:Employee) {
+  this.EmployeeService.deleteEmployeeid(employee.idemploye)
+    .subscribe(
+      data => {
+        console.log(data);
+        this.rechercherEmail.splice(this.rechercherEmail.indexOf(employee),1);
+        
+      },
+      error => console.log(error));
+     
 }
+//edite emplo
+editeEmploye()
+     {
+       
+     }
+
+
+}
+
+
