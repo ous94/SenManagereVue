92e71aaa44196c2ad8b5c50341141ec601194a41
Majanny <aisaisa02@gmail.com>
2019-01-15 15:09:21 +0000

    MAJ de tape-employee

--- src/app/employee/tape-employer/tape-employer.component.ts
+++ src/app/employee/tape-employer/tape-employer.component.ts
@@ -11,6 +11,9 @@ export class TapeEmployerComponent implements OnInit {
   showActionEmployee:boolean=false;
   showListerEmployee:boolean=false;
   showListerRecherche:boolean=false;
+  showListerRechercheSituation:boolean=false;
+  showListerRechercheReligion:boolean=false;
+  showListerRechercheEmail:boolean=false;
 
 
 
@@ -34,28 +37,39 @@ export class TapeEmployerComponent implements OnInit {
 
   visibiliteLister($event)
   {
-       this.showListerEmployee=!this.showListerEmployee
+       this.showListerEmployee=!this.showListerEmployee;
        this.showActionEmployee=false;
        this.showAjouterEmployye=false;
+       this.showListerRechercheSituation=false;
+       this.showListerRechercheReligion=false;
+       this.showListerRechercheEmail=false;
+
 
   }
 
   visibiliteAction($event)
   {
-    this.showActionEmployee=!this.showActionEmployee
+    this.showActionEmployee=!this.showActionEmployee;
        this.showAjouterEmployye=false;
        this.showListerEmployee=false;
        this.showListerRecherche=false;
+       this.showListerRechercheSituation=false;
+       this.showListerRechercheReligion=false;
+       this.showListerRechercheEmail=false;
+
 
   }
 
 
   visibiliteAjouter($event)
   {
-        this.showAjouterEmployye=!this.showAjouterEmployye
+        this.showAjouterEmployye=!this.showAjouterEmployye;
        this.showActionEmployee=false;
        this.showListerEmployee=false;
        this.showListerRecherche=false;
+       this.showListerRechercheSituation=false;
+       this.showListerRechercheReligion=false;
+       this.showListerRechercheEmail=false;
 
   }
   visibiliteRecherche($event)
@@ -64,7 +78,41 @@ export class TapeEmployerComponent implements OnInit {
        this.showAjouterEmployye=false;
        this.showListerEmployee=false;
        this.showListerRecherche=!this.showListerRecherche;
+       this.showListerRechercheSituation=false;
+       this.showListerRechercheReligion=false;
+       this.showListerRechercheEmail=false;
 
+
+  }
+
+  visibiliteSituation($event)
+  {
+       this.showListerEmployee=false;
+       this.showActionEmployee=false;
+       this.showAjouterEmployye=false;
+       this.showListerRechercheSituation=!this.showListerRechercheSituation;
+       this.showListerRechercheReligion=false;
+       this.showListerRechercheEmail=false;
+  }
+
+  visibiliteReligion($event)
+  {
+       this.showListerEmployee=false;
+       this.showActionEmployee=false;
+       this.showAjouterEmployye=false;
+       this.showListerRechercheSituation=false;
+       this.showListerRechercheReligion=!this.showListerRechercheReligion;
+       this.showListerRechercheEmail=false;
+  }
+
+  visibiliteEmail($event)
+  {
+       this.showListerEmployee=false;
+       this.showActionEmployee=false;
+       this.showAjouterEmployye=false;
+       this.showListerRechercheSituation=false;
+       this.showListerRechercheReligion=false;
+       this.showListerRechercheEmail=!this.showListerRechercheEmail;
   }
 
 }
